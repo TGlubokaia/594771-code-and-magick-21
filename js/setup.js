057ecebfaf35f5a50b3c7a6b82f7076423fe3257
TGlubokaia <64069375+TGlubokaia@users.noreply.github.com>
2020-10-13 15:06:14 +0300

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,27 +1,26 @@
 'use strict';
 
-
-const NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-const SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-const COLOR = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55), rgb(0, 0, 0)'];
-const EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-let numberOfPlayer = 4;
-let players = [];
+const wizardNames = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+const wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+const wizardCoatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55), rgb(0, 0, 0)'];
+const wizardEyes = ['black', 'red', 'blue', 'yellow', 'green'];
+const NUMBER_OF_PLAYERS = 4;
 
 document.querySelector('.setup').classList.remove('hidden');
 
-let similarListElement = document.querySelector('.setup-similar-list');
-let similarWizardTemplate = document.querySelector('#similar-wizard-template')
+const similarListElement = document.querySelector('.setup-similar-list');
+const similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-let getRandomNumber = function (min, max) {
+  const getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-let getPlayers = function (name, surname, color, eyes) {
-  for (let i = 0; i < numberOfPlayer; i++) {
-    let player = {};
+const getPlayers = function (name, surname, color, eyes) {
+  const players = [];
+  for (let i = 0; i < NUMBER_OF_PLAYERS; i++) {
+    const player = {};
     player.name = name[getRandomNumber(0, name.length)];
     player.surname = surname[getRandomNumber(0, surname.length)];
     player.coatColor = color[getRandomNumber(0, color.length)];
@@ -30,19 +29,19 @@ let getPlayers = function (name, surname, color, eyes) {
   }
   return players;
 };
-getPlayers(NAMES, SURNAMES, COLOR, EYES);
+const boxOfPlayers = getPlayers(wizardNames, wizardSurnames, wizardCoatColors, wizardEyes);
 
-let renderWizard = function (wizard) {
-  let wizardElement = similarWizardTemplate.cloneNode(true);
+const renderWizard = function (wizard) {
+  const wizardElement = similarWizardTemplate.cloneNode(true);
   wizardElement.querySelector('.setup-similar-label').textContent = wizard.name + ' ' + wizard.surname;
   wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
   return wizardElement;
 };
 
-let fragment = document.createDocumentFragment();
-for (let i = 0; i < players.length; i++) {
-  fragment.appendChild(renderWizard(players[i]));
+const fragment = document.createDocumentFragment();
+for (let i = 0; i < boxOfPlayers.length; i++) {
+  fragment.appendChild(renderWizard(boxOfPlayers[i]));
 }
 similarListElement.appendChild(fragment);
 
